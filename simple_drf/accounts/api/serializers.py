de2bde7f628a68e5dd704539d77ef56ecf083f52
Josh Karamuth <hello@joshkaramuth.com>
2025-01-03 13:55:20 +0400

    Get primary email for user

--- simple_drf/accounts/api/serializers.py
+++ simple_drf/accounts/api/serializers.py
@@ -4,7 +4,12 @@ from simple_drf.accounts.models import User
 
 
 class UserSerializer(serializers.ModelSerializer[User]):
+    email = serializers.SerializerMethodField()
+
+    def get_email(self, instance):
+        return instance.primary_email
+
     class Meta:
         model = User
-        fields = ["id", "username", "email", "first_name", "last_name", "date_joined"]
-        read_only_fields = ["id"]
+        fields = ["id", "username", "first_name", "last_name", "date_joined", "email"]
+        read_only_fields = ["id", "date_joined"]
--- simple_drf/accounts/models.py
+++ simple_drf/accounts/models.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from allauth.account.models import EmailAddress
+from django.utils.functional import cached_property
 
 
 class User(AbstractUser):
+    @cached_property
+    def primary_email(self):
+        email = EmailAddress.objects.get_primary(self)
+        if email is None:
+            email = self.email
+        else:
+            email = email.email
+        return email
+
     class Meta:
         ordering = ["-date_joined"]
         db_table = "users"
